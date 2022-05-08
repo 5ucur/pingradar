f356f9b8b950dfe8b7033e793cafe0b7c5194347
Grisha <grigorije05@gmail.com>
2022-05-08 16:37:48 +0200

    got math stuff working

--- pingradar.py
+++ pingradar.py
@@ -1,14 +1,52 @@
+import sys
+import time
+import socket
+import threading
+import subprocess
+
+import math
+from math import cos, sin
+
 import pygame
 
-pygame.init()
+def ping(ip):
+	result = ''
+	try:
+	    response = subprocess.check_output(
+	        ['ping', '-c', '1', ip],
+	        stderr=subprocess.STDOUT,  # get all output
+	        universal_newlines=True  # return string not bytes
+	    )
+	except subprocess.CalledProcessError:
+	    response = None
+	for word in response.split():
+		if "time=" in word:
+			result = word.strip("time=ms")
+	return int(result)
 
+class PingManager:
+	def __init__(self, ips):
+		self.ips = ips
+		self.threads = []
+
+ips = sys.argv[1:]
+
+pygame.init()
 FPS = 60
 clock = pygame.time.Clock()
 
-WIDTH = 1270
-HEIGHT = 720
+WIDTH = 640
+HEIGHT = 480
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping Radar")
+
+CIRCLE_R = HALF_HEIGHT - 20 #radius
+CIRCLE_CENTER = (HALF_WIDTH, HALF_HEIGHT) #x,y
+
+tick = 0
 
 running = True
 while running:
@@ -18,9 +56,17 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 
-	#game code
+	pygame.draw.circle(win, (0,255,0), CIRCLE_CENTER, CIRCLE_R, 1)
+	pygame.draw.circle(win, (0,255,0), CIRCLE_CENTER, 5, 5)
+
+	t_r = CIRCLE_R #test radius
+	angle = time.time() #in radians
+	p_x = t_r * cos(angle) + CIRCLE_CENTER[0]
+	p_y = t_r * sin(angle) + CIRCLE_CENTER[1]
+	pygame.draw.circle(win, (255,0,0), (p_x, p_y), 5, 5)
 
 	pygame.display.update()
+	tick += 1
 
 pygame.quit()
 quit()
